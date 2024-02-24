4a48919f67c6179789a799c38b7a5cf4056cb403
Nathan Arvan <nathannarvan@gamil.com>
2024-02-24 14:55:59 -0600

    fix typo

--- GameAPI/Program.cs
+++ GameAPI/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<GameContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("GameContext"));
+builder.Services.AddDbContext<GameContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("GameContext")));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
