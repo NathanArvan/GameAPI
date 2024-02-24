b08ba439618c731169ba7429ef4816067b843bb8
Nathan Arvan <nathannarvan@gamil.com>
2024-02-24 13:14:20 -0600

    create game context file

---
+++ GameInfrestructure/GameContext.cs
@@ -0,0 +1,16 @@
+﻿using GameDomain;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameInfrestructure
+{
+    public class GameContext
+    {
+        public DbSet<Ability> Abilities { get; set; }
+        public DbSet<Campaign> Campaigns { get; set; }
+        public DbSet<Character> Characters { get; set; } 
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Map> Maps { get; set; }
+        public DbSet<Token> Tokens { get; set; }
+    }
+}
