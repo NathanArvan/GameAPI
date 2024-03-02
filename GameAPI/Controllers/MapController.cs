5ed40e4a54ca07a2f0d902f34bd294422e9d2428
Nathan Arvan <nathannarvan@gamil.com>
2024-03-02 14:44:49 -0600

    add media type

--- GameAPI/Controllers/MapController.cs
+++ GameAPI/Controllers/MapController.cs
@@ -14,18 +14,21 @@ namespace GameAPI.Controllers
         }
 
         [HttpGet("maps")]
+        [Produces("application/json")]
         public async Task<List<Map>> GetMaps()
         {
             return await _mapService.GetMaps();
         }
 
         [HttpGet("maps/{id}")]
+        [Produces("application/json")]
         public async Task<Map> GetMap(int id)
         {
             return await _mapService.GetMap(id);
         }
 
         [HttpPost("maps")]
+        [Produces("application/json")]
         public async Task<Map> CreateMap(Map map)
         {
             return await _mapService.CreateMap(map);
