162982d92309fe12f5764d1f9e4c5306cf2a0eaf
Nathan Arvan <nathannarvan@gamil.com>
2024-03-02 14:44:58 -0600

    add cors policy

--- GameAPI/Program.cs
+++ GameAPI/Program.cs
@@ -4,7 +4,8 @@ using GameDomain.Tokens;
 using GameInfrestructure;
 using GameInfrestructure.Repositories;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.ClearProviders();
@@ -21,6 +22,13 @@ builder.Services.AddScoped<IMapRepsitory, MapRepository>();
 builder.Services.AddScoped<TokenService>();
 builder.Services.AddScoped<ITokenRepository, TokenRepository>();
 builder.Services.AddDbContext<GameContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("GameContext")));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+    {
+        policy.WithOrigins("http://localhost:4200");
+    });
+});
 var app = builder.Build();
 
 app.UseHttpLogging();
