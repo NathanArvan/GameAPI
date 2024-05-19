8f935993effc70132bcd356f1f1e6806320ba33b
Nathan Arvan <nathannarvan@gamil.com>
2024-05-19 13:27:46 -0500

    update abilitiy nullable fields

--- GameDomain/Abilities/Ability.cs
+++ GameDomain/Abilities/Ability.cs
@@ -7,15 +7,15 @@ namespace GameDomain.Abilities
     {
         public int AbilityId { get; set; }
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
         [NotMapped]
-        public object Target { get; set; } = new object();
+        public object? Target { get; set; } = new object();
         [NotMapped]
-        public object Effect { get; set; } = new object();
+        public object? Effect { get; set; } = new object();
         public int Range { get; set; }
         public int Duration { get; set; }
         [NotMapped]
         public object Requirements { get; set; } = new object();
-        public List<Item> Items { get; set; }
+        public List<Item>? Items { get; set; }
     }
 }
